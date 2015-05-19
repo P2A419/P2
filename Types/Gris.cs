78f78b7e57b3de1ca4f0827c35183b2d30a37e35
Bilal <bilal2644@gmail.com>
2015-05-20 00:13:18 +0200

    XD

---
+++ Types/Gris.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2.Types
+{
+    public class Gris : Type
+    {
+        public double Distance { get; set; }
+        public double Angle { get; set; }
+
+        public Gris(int Number, double Distance, double Angle)
+        {
+            this.Number = Number;
+            this.Distance = Distance;
+            this.Angle = Angle;
+        }
+    }
+}
---
+++ Wizard/Wizard.cs
@@ -0,0 +1,27 @@
+﻿using P2.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2.Wizards
+{
+    public class Wizard
+    {
+        public static List<Gris> Items = new List<Gris>();
+
+        public static void Start(string Filepath, string Algorhithm)
+        {
+            string[] lines = File.ReadAllLines(Filepath);
+            for (var i = 1; i == lines.Length; i++)
+            {
+                Gris g = new Gris(i, double.Parse(lines[0]), double.Parse(lines[1]));
+                Items.Add(g);
+            }
+
+
+        }
+    }
+}
